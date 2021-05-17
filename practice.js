d5351a93d0bf52ef1338d55d14cac5d64c05da27
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2021-05-18 08:13:49 +0900

    メニューバーと上に戻るボタンの実装

---
+++ practice.js
@@ -0,0 +1,22 @@
+$(function(){
+  $('.header-course').click(function(){
+    $('.header-toggle').slideToggle();
+  });
+});
+
+$(function(){
+  var pagetop  = $('#page_top');
+
+  pagetop.hide();
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+        pagetop.fadeIn();
+    } else {
+        pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function() {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+  });
+});
